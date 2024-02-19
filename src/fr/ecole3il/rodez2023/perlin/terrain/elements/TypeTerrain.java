8efe1b02e3ec49b648998a1aec0ac252c63be976
GCLEMENT3il <151721756+GCLEMENT3il@users.noreply.github.com>
2024-02-19 21:10:30 +0100

    Modification de typeterrain

--- src/fr/ecole3il/rodez2023/perlin/terrain/elements/TypeTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/TypeTerrain.java
@@ -1,13 +1,48 @@
 package fr.ecole3il.rodez2023.perlin.terrain.elements;
 
+import fr.ecole3il.rodez2023.perlin.Utils;
+
 import java.awt.image.BufferedImage;
 
-public enum TypeTerrain { // Enum car permet d'optimiser, cela permet de ne pas avoir une classe par valeur et permet de faire juste par exemple terrain.marais
+public enum TypeTerrain {
 
-    OCEAN, PLAINE, FORET_FEUILLUS, TOUNDRA, FORET_CONIFÈRES, MONTAGNE, DESERT, COLLINES, MARAIS;
+    OCEAN("ocean.png"),
+    PLAINE("plain.png"),
+    FORET_FEUILLUS("deciduous_forest.png"),
+    TOUNDRA("tundra.png"),
+    FORET_CONIFÈRES("coniferous_forest.png"),
+    MONTAGNE("mountain.png"),
+    DESERT("desert.png"),
+    COLLINES("hills.png"),
+    MARAIS("marsh.png");
+
+    private final String nomImage;
+
+    private BufferedImage image;
+
+    TypeTerrain(String nomImage) {
+        this.nomImage = nomImage;
+    }
 
     public BufferedImage getImage() {
+        if (image == null) {
+            image = Utils.chargerTuile(nomImage);
+        }
+        return image;
+    }
 
-        return null;
+    @Override
+    public String toString() {
+        return switch (this) {
+            case OCEAN -> "Océan";
+            case PLAINE -> "Plaine";
+            case FORET_FEUILLUS -> "Forêt de feuillus";
+            case TOUNDRA -> "Toundra";
+            case FORET_CONIFÈRES -> "Forêt de conifères";
+            case MONTAGNE -> "Montagne";
+            case DESERT -> "Désert";
+            case COLLINES -> "Collines";
+            case MARAIS -> "Marais";
+        };
     }
 }
